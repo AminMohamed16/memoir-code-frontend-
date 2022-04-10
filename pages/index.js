ed886b2ffef4c33763a58a9eb84dfd04d7d06da8
kitani <slmkitani@gmail.com>
2022-04-10 19:13:17 +0100

    set layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div className={styles.navbar}>navbar</div>
+      <div className={styles.container}>
+        <div className={styles.filter}>Filter</div>
+        <div className={styles.list}>List</div>
+        <div className={styles.map}>Map</div>
+      </div>
+    </div>
+  );
 }
