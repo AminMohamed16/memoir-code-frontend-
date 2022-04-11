c95113f90bc4fc512a8524fc13fcc477af35861d
kitani <slmkitani@gmail.com>
2022-04-11 01:23:21 +0100

    use EventList componet

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar/Navbar";
-
+import EventsList from "../components/EventsList/EventsList";
 export default function Home() {
   return (
     <div>
       <Navbar />
       <div className={styles.container}>
         <div className={styles.filter}>Filter</div>
-        <div className={styles.list}>List</div>
+        <div className={styles.list}>
+          <EventsList />
+        </div>
         <div className={styles.map}>Map</div>
       </div>
     </div>
